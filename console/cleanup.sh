077dea400acd8c5295059bf7f13a27ab9262adad
his4herhandez <rhb@ivfinance.mx>
2024-07-30 12:31:49 -0600

    nueva configuracion

---
+++ console/cleanup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Elimina archivos en el directorio de destino
+rm -rf /var/www/html/ec2-instance/*
+
+# O elimina el directorio completo si no es necesario
+# rm -rf /var/www/html/ec2-instance
+
+echo "Directorio de destino limpiado."
