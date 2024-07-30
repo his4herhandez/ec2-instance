8fa969dfc7212abb971cf8487101363ab803805a
his4herhandez <rhb@ivfinance.mx>
2024-07-30 16:17:02 -0600

    create sh to run php scripts

--- script/CreateHelloWorldMessage.php
+++ script/CreateHelloWorldMessage.php
@@ -1,16 +1,11 @@
 <?php
 // Ruta del directorio donde quieres crear el archivo
-$path = __DIR__ . '/models';
+$path = __DIR__ . '/';
 $fileName = 'hola_mundo.txt';
 $content = 'Hola Mundo';
 
-// Verifica si el directorio existe, si no, lo crea
-if (!is_dir($path)) {
-    mkdir($path, 0777, true);
-}
-
 // Ruta completa del archivo
-$path = $path . $nombreArchivo;
+$path = "$path/$nombreArchivo";
 
 // Crea el archivo y escribe el contenido
 file_put_contents($path, $content);
