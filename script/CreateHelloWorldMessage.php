ed3f04de7f135d940391ce8d291d6cc94fc84397
his4herhandez <rhb@ivfinance.mx>
2024-07-30 16:35:36 -0600

    create sh to run php scripts

--- script/CreateHelloWorldMessage.php
+++ script/CreateHelloWorldMessage.php
@@ -1,10 +1,9 @@
 <?php
 
 $path = __DIR__;
-$fileName = 'hola_mundo.txt'; // Asegúrate de que la variable se llame correctamente
-$fullPath = "$path/$fileName"; // Usa $fileName en lugar de $nombreArchivo
+$fileName = 'hola_mundo.txt';
+$fullPath = "$path/$fileName";
 
-// Crea o sobrescribe el archivo con el contenido 'Hola Mundo'
 file_put_contents($fullPath, 'Hola Mundo');
 
 echo "Archivo creado con éxito en: $fullPath";
