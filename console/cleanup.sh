6b9ad777eedf32e17a4c66da777965cb6dc7557c
his4herhandez <rhb@ivfinance.mx>
2024-07-30 15:31:46 -0600

    correction: se corrijo carga de documentos en modal de validacion de documentos

--- console/cleanup.sh
+++ console/cleanup.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-# Elimina archivos en el directorio de destino
+# Elimina todos los archivos en el directorio de destino
 rm -rf /var/www/html/ec2-instance/*
 
-# O elimina el directorio completo si no es necesario
-# rm -rf /var/www/html/ec2-instance
-
-echo "Directorio de destino limpiado."
+echo "Directorio limpiado antes del despliegue."
