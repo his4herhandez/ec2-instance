901e2ca16f80f4ec9eab16784987c83ac96823e0
Rigoberto Hernandez <rhb@ivfinance.mx>
2024-08-06 12:53:56 -0600

    configuracion de script para bucle for

--- scripts/update_repo.sh
+++ scripts/update_repo.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 
-cd /var/www/html/ec2-instance || exit 1
+# Lista de servidores
+directories=(
+  "/var/www/html/ec2-instance"
+  "/var/www/html/jenkins-two"
+  "/var/www/html/jenkins-three"
+  "/var/www/html/jenkins-three"
+)
 
-if [ -d ".git" ]; then # Verifica si el repositorio ya está clonado
-  git pull origin main
-else
-  echo "El directorio no es un repositorio Git."
-  exit 1
-fi
+# Iterar sobre cada directorio en la lista
+for dir in "${directories[@]}"; do
+  echo "Actualizando repositorio en $dir..."
 
-if [ $? -ne 0 ]; then
-  echo "Error al hacer git pull."
-  exit 1
-fi
+  cd "$dir" || { echo "No se pudo acceder al directorio $dir"; continue; }
 
-echo "Repositorio actualizado correctamente."
+  if [ -d ".git" ]; then # Verifica si el repositorio ya está clonado
+    git pull origin main
+
+    if [ $? -ne 0 ]; then
+      echo "Error al hacer git pull en $dir."
+      continue
+    fi
+
+    echo "Repositorio en $dir actualizado correctamente."
+  else
+    echo "El directorio $dir no es un repositorio Git."
+  fi
+done
