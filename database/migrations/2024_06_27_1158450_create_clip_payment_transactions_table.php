0d78c704b1cca071b5f46bea11a32b1cf0bbc84e
his4herhandez <rhb@ivfinance.mx>
2024-09-04 08:56:40 -0600

    create new tables

---
+++ database/migrations/2024_06_27_1158450_create_clip_payment_transactions_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+class CreateClipPaymentTransactionsTable extends Migration
+{
+    public function up()
+    {
+        Capsule::schema()->create('clip_payment_transactions', function (Blueprint $table) {
+            $table->unsignedInteger('id')->primary()->autoIncrement();
+            $table->string('clip_id', 60);
+            $table->unsignedInteger('financed_sale_id');
+            $table->decimal('amount', 10, 2);
+            $table->string('transac_status', 30)->nullable();
+            $table->string('description', 50)->nullable();
+            $table->string('payment_request_url', 120)->nullable();
+            $table->dateTime('expired_at')->nullable();
+            $table->text('payment_data')->nullable();
+            $table->timestamps();
+
+            // Definir las llaves foráneas (asegúrate de que las tablas referenciadas existen)
+            // $table->foreign('financed_sale_id')->references('id')->on('financed_saled')->onUpdate('cascade')->onDelete('restrict');
+        });
+    }
+
+    public function down()
+    {
+        Capsule::schema()->dropIfExists('clip_payment_transactions');
+    }
+}
